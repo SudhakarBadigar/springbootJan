015b7bfc8beaa878a657d377c9df1e7d2c5e6e6a
SudhakarBadigar <sudhakar.badigar87@gmail.com>
2024-01-10 16:42:07 +0530

    jan

---
+++ javaBasedConfiguration/src/main/java/in/nit/examples/JavaBasedConfigurationApplication.java
@@ -0,0 +1,13 @@
+package in.nit.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasedConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBasedConfigurationApplication.class, args);
+	}
+
+}
---
+++ javaBasedConfiguration/src/test/java/in/nit/examples/JavaBasedConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nit.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasedConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
